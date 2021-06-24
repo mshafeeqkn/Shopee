5c2e08b39d03ef22efa298ac48e37c0e6fad82ad
mshafeeqkn <mshafeeqkn@gmail.com>
2021-06-24 18:53:01 +0530

    Draggable list view

--- app/src/main/java/com/shafeeq/shopee/fragments/MainViewFragment.kt
+++ app/src/main/java/com/shafeeq/shopee/fragments/MainViewFragment.kt
@@ -1,66 +1,45 @@
 package com.shafeeq.shopee.fragments
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.CheckBox
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeAdapter
+import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeRecyclerView
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemDragListener
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemSwipeListener
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnListScrollListener
 import com.shafeeq.shopee.R
 
 class MainViewFragment : Fragment() {
-    private lateinit var itemList: ListView
+    private lateinit var mList: DragDropSwipeRecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main_view, container, false)
-        itemList = root.findViewById(R.id.itemList)
-        val adapter = ShopItemListAdapter(requireContext(), arrayListOf(
-            ListItem("Vegetables", SECTION),
-            ListItem("Uluva", ITEM),
-            ListItem("Kaduk", ITEM),
-            ListItem("Cheera", ITEM),
-            ListItem("Fruits", SECTION),
-            ListItem("Apple", ITEM),
-            ListItem("Orange", ITEM),
-            ListItem("Grape", ITEM),
-            ListItem("Rumman", ITEM),
-            ListItem("Grocery", SECTION),
-            ListItem("Rice", ITEM),
-            ListItem("Sugar", ITEM),
-            ListItem("Nuts", ITEM),
-            ListItem("Tea Powder", ITEM),
-            ListItem("Tooth Brush", ITEM),
-            ListItem("Macrona", ITEM),
-            ListItem("kadal", ITEM),
-            ListItem("parippu", ITEM),
-            ListItem("Vegetables", SECTION),
-            ListItem("Uluva", ITEM),
-            ListItem("Kaduk", ITEM),
-            ListItem("Cheera", ITEM),
-            ListItem("Fruits", SECTION),
-            ListItem("Apple", ITEM),
-            ListItem("Orange", ITEM),
-            ListItem("Grape", ITEM),
-            ListItem("Rumman", ITEM),
-            ListItem("Grocery", SECTION),
-            ListItem("Rice", ITEM),
-            ListItem("Sugar", ITEM),
-            ListItem("Nuts", ITEM),
-            ListItem("Tea Powder", ITEM),
-            ListItem("Tooth Brush", ITEM),
-            ListItem("Macrona", ITEM),
-            ListItem("kadal", ITEM),
-            ListItem("parippu", ITEM),
-        ))
-        itemList.adapter = adapter
+        val dataSet = listOf(
+            "Item 11", "Item 12", "Item 13",
+            "Item 21", "Item 22", "Item 23",
+            "Item 31", "Item 32", "Item 33",
+            "Item 41", "Item 42", "Item 43",
+            "Item 51", "Item 52", "Item 53",
+        )
+        val mAdapter = MyAdapter(dataSet)
+        mList = root.findViewById(R.id.list)
+        mList.layoutManager = LinearLayoutManager(requireContext())
+        mList.orientation = DragDropSwipeRecyclerView.ListOrientation.VERTICAL_LIST_WITH_VERTICAL_DRAGGING
+
+        mList.adapter = mAdapter
+        mList.swipeListener = onItemSwipeListener
+        mList.dragListener = onItemDragListener
+        mList.scrollListener = onListScrollListener
+
         return root
     }
 
@@ -69,56 +48,52 @@ class MainViewFragment : Fragment() {
     }
 }
 
-const val ITEM = 0
-const val SECTION = 1
+class MyAdapter(dataSet: List<String> = emptyList())
+    : DragDropSwipeAdapter<String, MyAdapter.ViewHolder>(dataSet) {
 
-data class ListItem(
-    var name: String = "",
-    var type: Int = ITEM
-)
-
-class ShopItemListAdapter(context: Context, private val data: ArrayList<ListItem>):
-        ArrayAdapter<ListItem>(context, R.layout.shop_item, data) {
-
-    private class ViewHolder(view: View?) {
-        var itemName: CheckBox? = null
-        var sectionName: TextView? = null
-
-        init {
-            itemName = view?.findViewById(R.id.itemName)
-            sectionName = view?.findViewById(R.id.sectionName)
-        }
+    class ViewHolder(itemView: View) : DragDropSwipeAdapter.ViewHolder(itemView) {
+        val itemText: TextView = itemView.findViewById(R.id.item_text)
+        val dragIcon: ImageView = itemView.findViewById(R.id.drag_icon)
     }
 
-    @SuppressLint("InflateParams")
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view: View
-        val viewHolder: ViewHolder
-        val listItem = data[position]
+    override fun getViewHolder(itemLayout: View) = MyAdapter.ViewHolder(itemLayout)
 
-        if(convertView == null) {
-            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            view = if(listItem.type == ITEM) inflater.inflate(R.layout.shop_item, null)
-                else inflater.inflate(R.layout.shop_list_seperator, null)
-
-            viewHolder = ViewHolder(view)
-            view.tag = viewHolder
-        } else {
-            view = convertView
-            viewHolder = view.tag as ViewHolder
-        }
-
-        if(listItem.type == ITEM) viewHolder.itemName?.text = listItem.name
-            else viewHolder.sectionName?.text = listItem.name
-
-        return view
+    override fun onBindViewHolder(item: String, viewHolder: MyAdapter.ViewHolder, position: Int) {
+        // Here we update the contents of the view holder's views to reflect the item's data
+        viewHolder.itemText.text = item
     }
 
-    override fun getViewTypeCount(): Int {
-        return 2
+    override fun getViewToTouchToStartDraggingItem(item: String, viewHolder: MyAdapter.ViewHolder, position: Int): View? {
+        // We return the view holder's view on which the user has to touch to drag the item
+        return viewHolder.dragIcon
+    }
+}
+
+private val onItemSwipeListener = object : OnItemSwipeListener<String> {
+    override fun onItemSwiped(position: Int, direction: OnItemSwipeListener.SwipeDirection, item: String): Boolean {
+        // Handle action of item swiped
+        // Return false to indicate that the swiped item should be removed from the adapter's data set (default behaviour)
+        // Return true to stop the swiped item from being automatically removed from the adapter's data set (in this case, it will be your responsibility to manually update the data set as necessary)
+        return false
+    }
+}
+
+private val onItemDragListener = object : OnItemDragListener<String> {
+    override fun onItemDragged(previousPosition: Int, newPosition: Int, item: String) {
+        // Handle action of item being dragged from one position to another
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return data[position].type
+    override fun onItemDropped(initialPosition: Int, finalPosition: Int, item: String) {
+        // Handle action of item dropped
+    }
+}
+
+private val onListScrollListener = object : OnListScrollListener {
+    override fun onListScrollStateChanged(scrollState: OnListScrollListener.ScrollState) {
+        // Handle change on list scroll state
+    }
+
+    override fun onListScrolled(scrollDirection: OnListScrollListener.ScrollDirection, distance: Int) {
+        // Handle scrolling
     }
 }
